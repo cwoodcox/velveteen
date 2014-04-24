8b60120a1fabde526f402c9f2e1b49cd6653a6a5
Corey Woodcox <corey.woodcox@gmail.com>
2014-04-24 16:18:20 -0600

    delayed queue and TESTS YAY

--- lib/velveteen/delayed_queue.rb
+++ lib/velveteen/delayed_queue.rb
@@ -1,15 +1,22 @@
 module Velveteen
   class DelayedQueue < Queue
-    DEFAULT_OPTIONS = {
-      arguments: {
-        'x-message-ttl' => (5*60*1000).to_s,
+    attr_accessor :delay
+
+    DEFAULT_OPTIONS = Configuration.new(durable: false)
+
+    def initialize delay, options = {}
+      @delay = delay
+      options[:arguments] = {
+        'x-message-ttl' => delay.to_s,
         'x-dead-letter-routing-key' => Velveteen.config.default_queue_name
       }
-    }
+
+      super "delay.#{delay}.milliseconds", DEFAULT_OPTIONS.deep_merge(options)
+    end
 
     def push job
       if queue.consumer_count > 0
-        logger.warn "Pushing a message on a delay queue with at least one active consumer. Job will be processed immediately."
+        logger.warn "Pushing a job on a delay queue with at least one active consumer. Job will be processed immediately."
       end
       super
     end
--- test/velveteen/test_delayed_queue.rb
+++ test/velveteen/test_delayed_queue.rb
@@ -2,6 +2,34 @@ require_relative '../test_velveteen'
 
 class TestDelayedQueue < Minitest::Test
   def test_queue
-    assert_kind_of Velveteen::Queue, Velveteen::DelayedQueue.new('delay.20.minutes')
+    assert_kind_of Velveteen::Queue, Velveteen::DelayedQueue.new(20*60*1000)
+  end
+
+  def test_queue_setup
+    mock_channel = Minitest::Mock.new
+    mock_channel.expect :queue, true do |name, options|
+      assert_equal name, "delay.30000.milliseconds"
+
+      assert_equal options.arguments['x-message-ttl'], '30000'
+      assert_equal options.arguments['x-dead-letter-routing-key'], ''
+    end
+
+    queue = Velveteen::DelayedQueue.new 30*1000
+    queue.instance_variable_set :@channel, mock_channel
+    queue.send :queue
+
+    assert mock_channel.verify
+  end
+
+  def test_publish
+    mock_queue = Minitest::Mock.new
+    mock_queue.expect :publish, true, [ String, Hash ]
+    mock_queue.expect :consumer_count, 0
+
+    queue = Velveteen::DelayedQueue.new 30*1000
+    queue.instance_variable_set :@queue, mock_queue
+    queue.push(some_job: 'options')
+
+    assert mock_queue.verify
   end
 end
