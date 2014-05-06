ee499b03ae5aa3ef20f98b5392b55ad944f14b79
Corey Woodcox <corey.woodcox@gmail.com>
2014-05-05 22:27:04 -0600

    test that the default queue works
    it needs to obey the config

--- test/test_velveteen.rb
+++ test/test_velveteen.rb
@@ -5,8 +5,20 @@ require 'minitest'
 require 'minitest/mock'
 
 class TestVelveteen < Minitest::Test
+  def setup
+    Velveteen.configure do |config|
+      config.default_queue_name = 'a.queue.name'
+      config.default_queue_options = { some: :options }
+    end
+  end
+
   def test_default_queue
-    assert_instance_of Velveteen::Queue, Velveteen.default_queue
+    queue = Velveteen.default_queue
+
+    assert_instance_of Velveteen::Queue, queue
+
+    assert_equal queue.name, 'a.queue.name'
+    assert_equal queue.options[:some], :options
   end
 
   def test_connection
@@ -17,5 +29,9 @@ class TestVelveteen < Minitest::Test
       Velveteen.connection
       assert bunny.verify
     end
+  end
+
+  def teardown
+    Velveteen.config = nil
   end
 end
